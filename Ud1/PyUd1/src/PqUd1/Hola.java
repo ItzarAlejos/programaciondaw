96d7ab54a56a34cce528662e98ad4dcf630a6308
ItzarAlejos <itzar.alejos@gmail.com>
2017-11-02 20:09:41 +0100

    Add files via upload

---
+++ Ud1/PyUd1/src/PqUd1/Hola.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PqUd1;
+
+/**
+ *
+ * @author daw1
+ */
+public class Hola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola clase");
+        //Esto muestra por pantalla el hola clase
+        
+        /*
+        
+        */
+         
+                
+    }
+    
+}
